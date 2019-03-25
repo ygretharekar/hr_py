7790bfe1e585054122d4b2ff062ebd10089cebbd
Yashawant Retharekar <yashwant.retharekar@gmail.com>
2019-03-25 09:05:51 +0530

    added test for bigger is greater

--- implementation/medium/test/test_bigger_is_greater.py
+++ implementation/medium/test/test_bigger_is_greater.py
@@ -1,21 +1,21 @@
 """test bigger is greater"""
 from .. import bigger_is_greater
-#ocsmerkgidvddsazqxjbqlrrxcotrnfvtnlutlfcafdlwiismslaytqdbvl
-#ocsmerkgidvddsazqxjbqlrrxcotrnfvtnlutlfcafdlwiismslaytqdbvl
-
 
 def test_solution():
     """Test solution"""
     b_g = bigger_is_greater.BiggerIsGreater()
-    # inp_a = "biehzcmjckznhwrfgglverxsezxuqpj"
-    # inp_b = "biehzcmjckznhwrfgglverxsjepquxz"
-    inp_a = "ocsmerkgidvddsazqxjbqlrrxcotrnfvtnlutlfcafdlwiismslaytqdbvl"
-    inp_b = "ocsmerkgidvddsazqxjbqlrrxcotrnfvtnlutlfcafdlwiismslaytqdbvl"
-    assert b_g.solution(inp_a) == inp_b
+    my_list = []
+    ans_list = []
+    with open('implementation\\medium\\test\\bigger_is_greater_input.txt', 'r') as f_1:
+        line = f_1.readline()
+        while line:
+            my_list.append(b_g.solution(line.strip()))
+            line = f_1.readline()
 
-def test_editorial_solution():
-    """Test solution"""
-    b_g = bigger_is_greater.BiggerIsGreater()
-    inp_a = "ocsmerkgidvddsazqxjbqlrrxcotrnfvtnlutlfcafdlwiismslaytqdbvl"
-    inp_b = "ocsmerkgidvddsazqxjbqlrrxcotrnfvtnlutlfcafdlwiismslaytqdbvl"
-    assert b_g.editorial_solution(inp_a) == inp_b
+    with open('implementation\\medium\\test\\bigger_is_greater_output.txt', 'r') as f_2:
+        line = f_2.readline()
+        while line:
+            ans_list.append(line.strip())
+            line = f_2.readline()
+
+    assert all([i == j for i, j in zip(my_list, ans_list)])
