23733f9802ada12a56d6ef29535ec377e445d849
Yashawant Retharekar <yashwant.retharekar@gmail.com>
2019-04-07 20:32:51 +0530

    bigger is greater test files moved to separate folder

--- implementation/medium/test/test_bigger_is_greater.py
+++ implementation/medium/test/test_bigger_is_greater.py
@@ -6,13 +6,15 @@ def test_solution():
     b_g = bigger_is_greater.BiggerIsGreater()
     my_list = []
     ans_list = []
-    with open('implementation\\medium\\test\\bigger_is_greater_input.txt', 'r') as f_1:
+    with open('implementation\\medium\\test\\bigger_is_greater_test_cases'
+              +'\\bigger_is_greater_input.txt', 'r') as f_1:
         line = f_1.readline()
         while line:
             my_list.append(b_g.solution(line.strip()))
             line = f_1.readline()
 
-    with open('implementation\\medium\\test\\bigger_is_greater_output.txt', 'r') as f_2:
+    with open('implementation\\medium\\test\\bigger_is_greater_test_cases'
+              +'\\bigger_is_greater_output.txt', 'r') as f_2:
         line = f_2.readline()
         while line:
             ans_list.append(line.strip())
