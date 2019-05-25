706f29cdbe270ca8e924626cc9df70205152cead
Yashawant Retharekar <yashwant.retharekar@gmail.com>
2019-05-25 14:22:40 +0530

    some changes

--- implementation/medium/test/test_the_grid_search.py
+++ implementation/medium/test/test_the_grid_search.py
@@ -10,17 +10,17 @@ def test_solution():
     pattern = []
 
     with open('implementation\\medium\\test\\the_grid_search_test_cases'
-              +'\\the_grid_search_input.txt', 'r') as f_1:
+              + '\\the_grid_search_input.txt', 'r') as f_1:
         line = f_1.readline()
         while line:
             grid.append(str(line).strip())
             line = f_1.readline()
 
     with open('implementation\\medium\\test\\the_grid_search_test_cases'
-              +'\\the_grid_search_input2.txt', 'r') as f_2:
+              + '\\the_grid_search_input2.txt', 'r') as f_2:
         line = f_2.readline()
         while line:
             pattern.append(str(line).strip())
             line = f_2.readline()
 
-    assert grid_search.solution(grid, pattern) == "NO"
+    assert grid_search.solution(grid, pattern) == "YES"
