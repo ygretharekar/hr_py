b36bec3afd6be450528396161fa0de0afaf42d95
Yashawant Retharekar <yashwant.retharekar@gmail.com>
2019-07-04 22:33:58 +0530

    Workaround got 60 points when ran on pypy 3

--- strings/medium/common_child.py
+++ strings/medium/common_child.py
@@ -5,15 +5,17 @@ class CommonChild:
     """docstring"""
     def solution(self, a_str, b_str):
         """docstring"""
-        d_p = [[0 for _ in range(len(a_str) + 1)] for __ in range(len(b_str) + 1)]
+        # d_p = [[0] * (len(a_str) + 1)  for __ in range(len(b_str) + 1)]
+        d_p = [[0] * (len(a_str) + 1)  for _ in range(2)]
 
+        for i in range(len(a_str)):
+            a_0 = i % 2
+            a_1 = (i + 1) % 2
 
-
-        for i, x_char in enumerate(a_str):
-            for j, y_char in enumerate(b_str):
-                if x_char == y_char:
-                    d_p[i + 1][j + 1] = d_p[i][j] + 1
+            for j in range(len(b_str)):
+                if a_str[i] == b_str[j]:
+                    d_p[a_1][j + 1] = d_p[a_0][j] + 1
                 else:
-                    d_p[i + 1][j + 1] = max(d_p[i][j + 1], d_p[i + 1][j])
+                    d_p[a_1][j + 1] = max(d_p[a_0][j + 1], d_p[a_1][j])
 
-        return d_p[-1][-1]
+        return d_p[(i + 1) % 2][-1]
