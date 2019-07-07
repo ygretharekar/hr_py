01090dc099ab2d05dc2f8c784462a07ec54dd7ff
Yashawant Retharekar <yashwant.retharekar@gmail.com>
2019-07-07 23:20:26 +0530

    [WIP] bear and steady gene

--- strings/medium/bear_and_steady_gene.py
+++ strings/medium/bear_and_steady_gene.py
@@ -6,6 +6,27 @@ class BearAndSteadyGene:
     def solution(self, gene):
         """docstring"""
 
-        mx = len(gene) // 4
+        gene_length = len(gene)
+
+        cnt = {'A': 0, 'C': 0, 'G':0, 'T': 0}
+
+        min_length = gene_length - 1
+
+        for j,  letter in enumerate(gene[::-1]):
+            if cnt[letter] == gene_length // 4:
+                break
+
+            cnt[letter] = cnt[letter] + 1
+
+
+        j = gene_length - j - 1
+
+        min_length = j
+
+        for i, letter in enumerate(gene):
+            cnt[letter] = cnt[letter] + 1
+
+            while j < gene_length and cnt[letter] > gene_length // 4:
+                pass
 
         return gene
